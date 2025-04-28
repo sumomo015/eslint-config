49e86c1f49748ae124f6399093ec388c0a8636d5
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2025-04-28 19:14:41 +0900

    feat: migrate configuration from tsup to tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsdown'
+
+import type { UserConfig } from 'tsdown'
+
+const config: UserConfig = defineConfig({
+  entry: ['src/index.ts'],
+  format: 'esm',
+  clean: true,
+  dts: true,
+})
+
+export default config
