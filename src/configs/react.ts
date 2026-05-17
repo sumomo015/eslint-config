dcd952f6eec47f02e5ac0dc8f34d15b3c1910276
sumomo015 <138793740+sumomo015@users.noreply.github.com>
2026-05-17 23:25:42 +0900

    chore: update pnpm workspace settings and ESLint configurations
    - Set 'unrs-resolver' to false in pnpm-workspace.yaml to prevent builds.
    - Reorder ESLint configurations in react.ts to include 'strict-type-checked' and 'disable-conflict-eslint-plugin-react-hooks'.
    - Remove 'no-magic-numbers' rule from typescript.ts and adjust related settings.

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -5,11 +5,12 @@ import eslintNext from '@next/eslint-plugin-next'
 import type { Options, RawConfig } from '../types'
 
 const reactConfigs: RawConfig[] = [
-  eslintReact.configs['strict-type-checked'],
   {
     ...eslintReactHooks.configs.flat.recommended,
     name: 'react-hooks',
   },
+  eslintReact.configs['strict-type-checked'],
+  eslintReact.configs['disable-conflict-eslint-plugin-react-hooks'],
 ]
 
 const nextConfig: RawConfig = eslintNext.configs['core-web-vitals']
